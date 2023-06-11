64317ef49903b71933bee3e349c60a86b5853170
Akash Suklabaidya <akashsuklabaidya593@gmail.com>
2023-06-11 11:44:21 +0530

    Commited

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,86 +1,3 @@
-// import React from 'react';
-// import InstagramIcon from '@mui/icons-material/Instagram';
-// import FacebookIcon from '@mui/icons-material/Facebook';
-// import LinkedInIcon from '@mui/icons-material/LinkedIn';
-// import GitHubIcon from '@mui/icons-material/GitHub';
-// import { Box } from '@mui/material';
-
-// const Footer = () => {
-//     return (
-//         <Box
-//             sx={{
-//                 left: 0,
-//                 width: "100%",
-//                 height: "auto",
-//                 paddingTop: "3rem",
-//                 bottom: "80px"
-
-//             }}
-//         >
-//             <footer style={styles.footer}>
-//                 <p style={styles.text}>
-//                     &copy; {new Date().getFullYear()}
-//                     <span style={styles.brand} > VisualForces.</span>
-//                     All rights reserved.
-//                 </p>
-//                 <p style={styles.text}>
-//                     Developed with love ❤️ by <a style={styles.link} target="_blank" rel="noopener noreferrer" href='https://www.linkedin.com/in/akash-suklabaidya-435aa5227/'
-//                     >Akash</a>
-//                 </p>
-//                 <p  >
-//                     <a style={styles.linkIcon} href='https://www.instagram.com/akash_sb__/' target="_blank" rel="noopener noreferrer" >
-//                         <InstagramIcon />
-//                     </a>
-//                     <a style={styles.linkIcon} href='https://www.facebook.com/akash.suklabaidya.31/' target="_blank" rel="noopener noreferrer" >
-//                         <FacebookIcon />
-//                     </a>
-//                     <a style={styles.linkIcon} href='https://www.linkedin.com/in/akash-suklabaidya-435aa5227/' target="_blank" rel="noopener noreferrer" >
-//                         <LinkedInIcon />
-//                     </a>
-//                     <a style={styles.linkIcon} href='https://github.com/akashsb18' target="_blank" rel="noopener noreferrer" >
-//                         <GitHubIcon />
-//                     </a>
-//                 </p>
-//             </footer>
-//         </Box>
-//     );
-// };
-
-// const styles = {
-//     footer: {
-//         backgroundColor: '#f5f5f5',
-//         padding: '20px',
-//         textAlign: 'center',
-//     },
-//     text: {
-//         fontSize: '14px',
-//         color: '#555555',
-//         marginBottom: '8px',
-//     },
-//     link: {
-//         color: '#0076cc',
-//         textDecoration: 'none',
-//         marginRight: '8px',
-//     },
-//     linkIcon: {
-//         color: '#555555',
-//         textDecoration: 'none',
-//         marginRight: '8px',
-//     },
-//     brand: {
-//         fontSize: '25px'
-//     }
-// };
-
-// export default Footer;
-
-
-
-
-
-
-
-
 import React from 'react';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
@@ -90,61 +7,42 @@ import { Box } from '@mui/material';
 
 const Footer = () => {
     return (
-        <footer style={styles.footer}>
-            <Box sx={{ flexGrow: 1 }} />
-            <Box sx={{ p: 2 }}>
+        <Box
+            sx={{
+                left: 0,
+                width: "100%",
+                height: "auto",
+                paddingTop: "3rem",
+                bottom: "80px"
+
+            }}
+        >
+            <footer style={styles.footer}>
                 <p style={styles.text}>
                     &copy; {new Date().getFullYear()}
-                    <span style={styles.brand}> VisualForces.</span>
+                    <span style={styles.brand} > VisualForces.</span>
                     All rights reserved.
                 </p>
                 <p style={styles.text}>
-                    Developed with love ❤️ by{' '}
-                    <a
-                        style={styles.link}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        href="https://www.linkedin.com/in/akash-suklabaidya-435aa5227/"
-                    >
-                        Akash
-                    </a>
+                    Developed with love ❤️ by <a style={styles.link} target="_blank" rel="noopener noreferrer" href='https://www.linkedin.com/in/akash-suklabaidya-435aa5227/'
+                    >Akash</a>
                 </p>
-                <p>
-                    <a
-                        style={styles.linkIcon}
-                        href="https://www.instagram.com/akash_sb__/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
+                <p  >
+                    <a style={styles.linkIcon} href='https://www.instagram.com/akash_sb__/' target="_blank" rel="noopener noreferrer" >
                         <InstagramIcon />
                     </a>
-                    <a
-                        style={styles.linkIcon}
-                        href="https://www.facebook.com/akash.suklabaidya.31/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
+                    <a style={styles.linkIcon} href='https://www.facebook.com/akash.suklabaidya.31/' target="_blank" rel="noopener noreferrer" >
                         <FacebookIcon />
                     </a>
-                    <a
-                        style={styles.linkIcon}
-                        href="https://www.linkedin.com/in/akash-suklabaidya-435aa5227/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
+                    <a style={styles.linkIcon} href='https://www.linkedin.com/in/akash-suklabaidya-435aa5227/' target="_blank" rel="noopener noreferrer" >
                         <LinkedInIcon />
                     </a>
-                    <a
-                        style={styles.linkIcon}
-                        href="https://github.com/akashsb18"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
+                    <a style={styles.linkIcon} href='https://github.com/akashsb18' target="_blank" rel="noopener noreferrer" >
                         <GitHubIcon />
                     </a>
                 </p>
-            </Box>
-        </footer>
+            </footer>
+        </Box>
     );
 };
 
@@ -153,8 +51,6 @@ const styles = {
         backgroundColor: '#f5f5f5',
         padding: '20px',
         textAlign: 'center',
-        position: 'sticky',
-        bottom: 0,
     },
     text: {
         fontSize: '14px',
@@ -172,8 +68,14 @@ const styles = {
         marginRight: '8px',
     },
     brand: {
-        fontSize: '25px',
-    },
+        fontSize: '25px'
+    }
 };
 
 export default Footer;
+
+
+
+
+
+
